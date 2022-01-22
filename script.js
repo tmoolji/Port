e0d7497fcb0206d6a1dad9930771781c3a80fba5
Taimur Moolji <Taimur Moolji>
2022-01-21 23:08:57 -0500

    expierimented with anime.js and framework for website

---
+++ script.js
@@ -0,0 +1,23 @@
+const anime = require('https://cdn.jsdelivr.net/npm/animejs@3.0.1/lib/anime.js');
+
+
+
+const anim = anime.timeline({
+  loop: true,
+  direction: 'alternate',
+});
+
+anim
+  .add({
+    targets: '#hexagon path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutQuart',
+    duration: 2000,
+    delay: function(el, i) { return i * 250 },
+  })
+  .add({
+    targets: '#hexagon #B',
+    duration: 1000,
+    opacity: 1,
+    easing: 'easeInOutQuart'
+  });
